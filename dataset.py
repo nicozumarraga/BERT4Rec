1a3062cc5001af3ff83ac8d490283ef547b8fd4e
Nico Zumarraga <nicolaszumarraga9@gmail.com>
2025-04-19 18:52:04 +0200

    data preprocessing

--- dataset.py
+++ dataset.py
@@ -6,12 +6,8 @@ import random
 
 class Dataset:
     def __init__(self, path):
-        random.seed(42)
-        np.random.seed(42)
-        torch.manual_seed(42)
-
         self.path = path
-        self.ratings_path = os.path.join(path, "ratings.dat")
+        self.ratings_path = os.path.join(self.path, "ratings.dat")
         self.ratings = self.preprocess_ratings(self.load_ratings_as_df(self.ratings_path))
 
     def load_ratings_as_df(self, filename):
@@ -25,26 +21,32 @@ class Dataset:
 
     def preprocess_ratings(self, ratings):
         ratings_processed = ratings.copy()
-        ratings_processed = self.convert_to_binary(ratings_processed)
-        ratings_processed = self.filter_less_than_5(ratings_processed)
-        ratings_processed = self.order_by_timestamp(ratings_processed)
-        #ratings_processed = self.perform_sequence_truncation(ratings_processed)
-        #ratings_processed = self.split_train_test(ratings_processed)
+        ratings_processed = self._filter_less_than_4(ratings_processed)
+        ratings_processed.loc[:, 'rating'] = 1 # convert to binary signal
+        ratings_processed = self._map_ratings_to_dense_sequence(ratings_processed)
+        ratings_processed = self._filter_less_than_5_interactions(ratings_processed)
+        ratings_processed = self._reformat_into_list_and_drop_others(ratings_processed)
         return ratings_processed
 
-    def convert_to_binary(self, ratings):
-        ratings = ratings.loc[ratings['rating'] >= 4]
-        ratings.loc[:, 'rating'] = 1
+    def _filter_less_than_4(self, ratings):
+        return ratings.loc[ratings.rating >= 4]
+
+    def _map_ratings_to_dense_sequence(self, ratings):
+        """
+        Used to map the movie_id to a dense sequence for better training performance.
+        We reserve the first two ids for padding and `MASKED` values.
+        """
+        dense_movie_ids = {k:i+2 for i, k in enumerate(sorted(list(ratings.movie_id.unique())))}
+        ratings["dense_movie_id"] = ratings["movie_id"].map(dense_movie_ids)
         return ratings
 
-    def order_by_timestamp(self, ratings):
-        return ratings.sort_values(by=['user_id', 'timestamp'], ascending=True)
+    def _filter_less_than_5_interactions(self, ratings):
+        user_interactions = ratings.groupby('user_id').size()
+        out_users = user_interactions[user_interactions < 5].index.tolist()
+        ratings = ratings[~ratings['user_id'].isin(out_users)]
+        return ratings
 
-    def filter_less_than_5(self, ratings):
-        return ratings.groupby('user_id').filter(lambda x: len(x) >= 5)
-
-    def perform_sequence_truncation(self, ratings, sequence_length: int = 20):
-        pass
-
-    def split_train_test(self, ratings, train: float = 0.7, val: float = 0.15, test: float = 0.15):
-        pass
+    def _reformat_into_list_and_drop_others(self, ratings):
+        ratings.sort_values(by=['timestamp'], inplace=True)
+        ratings = ratings.groupby('user_id').agg(list).drop(['rating', 'timestamp','movie_id'], axis=1)
+        return ratings
